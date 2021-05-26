20b70f9898fcc2da3ba76340d921f35b07dac61d
Thang Do Minh <44288750+dominhthang1999@users.noreply.github.com>
2021-05-26 18:07:22 +0700

    Doing gini

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ training_data = [
 header = ["color", "diameter", "label"]
 
 
-def unique_vals(rows, col):
+def unique_values(rows, col):
     """ Find the unique values for a column in a dataset. """
     return set([row[col] for row in rows])
 
@@ -20,7 +20,7 @@ def unique_vals(rows, col):
 class Question:
     """ A Question is used to partition a dataset """
 
-    def __init__(self, column, value) -> None:
+    def __init__(self, column: int, value) -> None:
         self.column = column
         self.value = value
 
@@ -40,8 +40,14 @@ class Question:
 
 
 def class_counts(rows):
-    pass
-
+    labels = {}
+    for row in rows:
+        name_label = row[-1]
+        if not name_label in labels:
+            labels[name_label] = 1
+            continue
+        labels[name_label] += 1
+    return labels
 
 class Leaf:
     """ A Leaf node classifies data. """
@@ -58,6 +64,16 @@ class Decision_Node:
         self.true_branch = true_branch
         self.false_branch = false_branch
 
+def gini(rows):
+
+    labels = class_counts(rows)
+    impurity = 1
+    for label in labels:
+
+    return
+
+def info_gain(left, right, current_uncertainty):
+    return
 
 def find_best_split(rows):
     """ 
@@ -66,15 +82,44 @@ def find_best_split(rows):
     """
     best_gain = 0
     best_question = None
+    current_uncertainty = gini(rows)
+    number_column = len(header)
 
-    for column in rows:
-        print('test %s' % column)
+    for column in range(number_column):
+        values = unique_values(rows, column)
+        for value in values:
+            question = Question(column, value)
+
+            # Splitting the dataset
+            true_rows, false_rows = partition(rows, question)
+
+            # Skip the split if it does not divide the dataset.
+            if len(true_rows) == 0 or len(false_rows) == 0:
+                continue
+
+            # Calculate the information gain from this split
+            gain = info_gain
+
+
+            # print(' %s: %s ' % (column, values) )
 
     return best_gain, best_question
 
 
 def partition(rows, question):
-    return None, None
+    '''
+    For each row in the dataset, check if it matches the question.
+    If so, add it into "true_rows", otherwise, add it to "false_rows"
+    '''
+    true_rows, false_rows = [], []
+
+    for row in rows:
+        if question.match(row):
+            true_rows.append(row)
+        else:
+            false_rows.append(row)
+
+    return true_rows, false_rows
 
 
 def build_tree(rows):
@@ -100,7 +145,7 @@ def build_tree(rows):
 
 if __name__ == '__main__':
 
-    my_tree = build_tree(training_data)
+    # my_tree = build_tree(training_data)
 
     # Evaluate
     testing_data = [
@@ -111,7 +156,9 @@ if __name__ == '__main__':
         ['Yellow', 3, 'Lemon'],
     ]
 
-    # print(testing_data)
+    test = class_counts(testing_data)
+
+    print(test)
     print(Question(0, 'Green'))
     print(Question(1, 3))
 
