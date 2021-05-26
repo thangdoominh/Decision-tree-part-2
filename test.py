002d890b5d6462d7a67a9a5a2f6d5202181f703c
Thang Do Minh <44288750+dominhthang1999@users.noreply.github.com>
2021-05-26 23:59:44 +0700

    Need predict

--- test.py
+++ test.py
@@ -53,7 +53,7 @@ class Leaf:
     """ A Leaf node classifies data. """
 
     def __init__(self, rows) -> None:
-        self.prediction = class_counts(rows)
+        self.predictions = class_counts(rows)
 
 
 class Decision_Node:
@@ -68,42 +68,52 @@ def gini(rows):
 
     labels = class_counts(rows)
     impurity = 1
-    for label in labels:
-
-    return
+    len_rows = len(rows)    # Total current sample 
+    for name_label in labels:
+        probability_of_label = labels[name_label] / len_rows
+        impurity -= probability_of_label ** 2
+    return impurity
 
 def info_gain(left, right, current_uncertainty):
-    return
+    # left is true_rows
+    # right is false_rows
+    info = 0    
+    
+    p_left = len(left) / (len(left) + len(right)) # probability left
+    p_right = 1 - p_left  # probability right
+    info = current_uncertainty - (p_left * gini(left) + p_right * gini(right))
+    return info
+
 
 def find_best_split(rows):
     """ 
     Find the best question to ask by iterating over every feature
     and calculating the information gain
     """
-    best_gain = 0
+    best_info_gain = 0
     best_question = None
     current_uncertainty = gini(rows)
-    number_column = len(header)
+    number_column = len(header) - 1
 
     for column in range(number_column):
         values = unique_values(rows, column)
         for value in values:
-            question = Question(column, value)
+            _question = Question(column, value)
 
             # Splitting the dataset
-            true_rows, false_rows = partition(rows, question)
+            true_rows, false_rows = partition(rows, _question)
 
             # Skip the split if it does not divide the dataset.
             if len(true_rows) == 0 or len(false_rows) == 0:
                 continue
 
             # Calculate the information gain from this split
-            gain = info_gain
+            _info_gain = info_gain(true_rows, false_rows, current_uncertainty)
 
+            if _info_gain >= best_info_gain:
+                best_info_gain, best_question = _info_gain, _question
 
-            # print(' %s: %s ' % (column, values) )
-
-    return best_gain, best_question
+    return best_info_gain, best_question
 
 
 def partition(rows, question):
@@ -120,6 +130,21 @@ def partition(rows, question):
             false_rows.append(row)
 
     return true_rows, false_rows
+
+def classify(row, node):
+    """See the 'rules of recursion' above."""
+
+    # Base case: we've reached a leaf
+    if isinstance(node, Leaf):
+        return node.predictions
+
+    # Decide whether to follow the true-branch or the false-branch.
+    # Compare the feature / value stored in the node,
+    # to the example we're considering.
+    if node.question.match(row):
+        return classify(row, node.true_branch)
+    else:
+        return classify(row, node.false_branch)
 
 
 def build_tree(rows):
@@ -142,10 +167,29 @@ def build_tree(rows):
 
     return Decision_Node(question, true_branch, false_branch)
 
+def print_tree(node, spacing=""):
+    """World's most elegant tree printing function."""
+
+    # Base case: we've reached a leaf
+    if isinstance(node, Leaf):
+        print (spacing + "Predict", node.predictions)
+        return
+
+    # Print the question at this node
+    print (spacing + str(node.question))
+
+    # Call this function recursively on the true branch
+    print (spacing + '--> True:')
+    print_tree(node.true_branch, spacing + "  ")
+
+    # Call this function recursively on the false branch
+    print (spacing + '--> False:')
+    print_tree(node.false_branch, spacing + "  ")
+
 
 if __name__ == '__main__':
 
-    # my_tree = build_tree(training_data)
+    my_tree = build_tree(training_data)
 
     # Evaluate
     testing_data = [
@@ -156,10 +200,8 @@ if __name__ == '__main__':
         ['Yellow', 3, 'Lemon'],
     ]
 
-    test = class_counts(testing_data)
-
-    print(test)
-    print(Question(0, 'Green'))
-    print(Question(1, 3))
+    print_tree(my_tree)
+    # print(Question(0, 'Green'))
+    # print(Question(1, 3))
 
 
